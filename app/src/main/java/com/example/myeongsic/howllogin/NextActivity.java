00c5454b62e3bfcdc5a21329e7bd15f61450a4ae
MyeongSic <you6878@icloud.com>
2017-05-02 23:06:15 +0900

    init

---
+++ app/src/main/java/com/example/myeongsic/howllogin/NextActivity.java
@@ -0,0 +1,32 @@
+package com.example.myeongsic.howllogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+
+        final TextView textView = (TextView)findViewById(R.id.user_name);
+
+        textView.setText(getIntent().getStringExtra("id"));
+
+        Button button =(Button)findViewById(R.id.close_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("logout_user", textView.getText().toString()+"님이 로그아웃 하셨습니다.");
+                setResult(20,intent);
+                finish();
+            }
+        });
+    }
+}
